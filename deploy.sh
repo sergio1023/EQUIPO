a6ade2e60c3a6310b4f94d6ee1ba6f8b0053e1e0
sergio1023 <sergyfutbol37@gmail.com>
2021-11-15 11:27:20 +0000

    menu

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ clear
 	##############
 # Dejar unas cuantes líneas de margen ya que al editar la misma línea habrá conflicto 
 #y no se subirán los cambios
-useradd juanki
 
 
 
@@ -17,4 +16,17 @@ useradd juanki
 
 
 #A partir de  Sergio expondrá sus ideas
-useradd sergio
+#Los usuarios con UUID menor de 1000 serán usuarios creados después de la instalación 
+#del sistema
+
+
+
+
+
+
+########################
+#COMIENZO DEL EJERCICIO#
+########################
+#1. Lista usuarios
+#read (listar usuarios){}
+cut -d: -f1,3 /etc/passwd | egrep ':[0-9]{4}$' | cut -d: -f1
