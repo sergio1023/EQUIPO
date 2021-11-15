b8dedb8974aa5f30f7ee808708e69180249084d3
sergio1023 <sergyfutbol37@gmail.com>
2021-11-15 10:41:56 +0100

    MENÚ USUARIOS

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+clear
+
+#A partir de aquí Juan Carlos expondrá sus ideas con sus comentarios correspondientes
+# y todo el código propuesto irá sin comentar
+	##############
+	##IMPORTANTE##
+	##############
+# Dejar unas cuantes líneas de margen ya que al editar la misma línea habrá conflicto 
+#y no se subirán los cambios
+
+
+
+
+
+
+
+
+#A partir de  Sergio expondrá sus ideas
